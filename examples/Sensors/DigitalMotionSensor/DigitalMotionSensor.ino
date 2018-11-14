451f14d9e9e3d6ccefa989a41727421ab6332371
shramik salgaonkar <shramiksalgaonkar@yahoo.com>
2018-11-14 23:24:56 +0530

    Update DigitalMotionSensor.ino

--- examples/Sensors/DigitalMotionSensor/DigitalMotionSensor.ino
+++ examples/Sensors/DigitalMotionSensor/DigitalMotionSensor.ino
@@ -55,7 +55,7 @@ void checkSensor()
 		// Check the sensor state and send data when it changes.
 		currentState = digitalRead(SENSOR_PIN);
 		if (currentState != previousState) {
-                        Cayenne.virtualWrite(VIRTUAL_CHANNEL, currentState, "digital_sensor","d");
+                       Cayenne.virtualWrite(VIRTUAL_CHANNEL, currentState, "digital_sensor","d");
 			previousState = currentState;
 		}
         previousMillis = currentMillis;
