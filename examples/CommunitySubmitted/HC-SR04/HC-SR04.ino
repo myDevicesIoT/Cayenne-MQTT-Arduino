4fc320c6c0585f36de7a3150ae40fa7d10c89b70
shramik salgaonkar <shramiksalgaonkar@yahoo.com>
2019-02-07 16:15:46 +0530

    Update HC-SR04.ino

--- examples/CommunitySubmitted/HC-SR04/HC-SR04.ino
+++ examples/CommunitySubmitted/HC-SR04/HC-SR04.ino
@@ -6,10 +6,10 @@
   1. Attach a HC-SR04 to your Arduino:
    Schematic:
    HC-SR04    Arduino
-   [VDD] --- [5V]
+   [VCC] --- [5V]
    [GND] --- [GND]
-   [TRIGGER] --- [Digital Pin 2]
-   [ECHO] --- [Digital Pin 3]
+   [TRIGGER_PIN] --- [Digital Pin 2]
+   [ECHO_PIN] --- [Digital Pin 3]
   2. Set the DISTANCE_VIRTUAL_CHANNEL value below to a free virtual channel.
   3. Set the Cayenne authentication info to match the authentication info from the Dashboard.
   4. Compile and upload this sketch.
@@ -28,17 +28,17 @@ char clientID[] = "CLIENT_ID";
 
 #define DISTANCE_VIRTUAL_CHANNEL 1
 
-// defines pins numbers
-#define TRIGPIN  2
-#define ECHOPIN 3
+// Defines pins numbers for the HC-SR04 connections.
+#define TRIGGER_PIN  2
+#define ECHO_PIN 3
 
-// defines variables
+// Defines variables for storing the calculated values.
 long duration;
 int distance;
 
 void setup() {
-  pinMode(TRIGPIN, OUTPUT);
-  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   Serial.begin(9600);
   Cayenne.begin(username, password, clientID);
 }
@@ -50,17 +50,19 @@ void loop() {
 // This function is called at intervals to send temperature sensor data to Cayenne.
 CAYENNE_OUT(DISTANCE_VIRTUAL_CHANNEL)
 {
-  digitalWrite(TRIGPIN, LOW);// Clears the trigPin
+  digitalWrite(TRIGGER_PIN, LOW);// Clears the trigPin
   delayMicroseconds(2);
 
-  digitalWrite(TRIGPIN, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(TRIGGER_PIN, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
   delayMicroseconds(10);
-  digitalWrite(TRIGPIN, LOW);
-  
-  duration = pulseIn(ECHOPIN, HIGH);// Reads the echoPin, returns the sound wave travel time in microseconds
+  digitalWrite(TRIGGER_PIN, LOW);
 
-  distance = duration * 0.034 / 2;  // Calculating the distance
+  duration = pulseIn(ECHO_PIN, HIGH);// Reads the echoPin, returns the sound wave travel time in microseconds
 
-  // Send the distance value to Cayenne on counter widget.
-  Cayenne.virtualWrite(DISTANCE_VIRTUAL_CHANNEL, distance, "counter", "null");
+  distance = duration * 0.034 / 2;  // Calculating the distance.
+
+  // Send the distance value to Cayenne on proximity widget in centimeter.
+  Cayenne.virtualWrite(DISTANCE_VIRTUAL_CHANNEL, distance, "prox", "cm");
+  // Send the distance value to Cayenne on proximity widget in meter.
+  //Cayenne.virtualWrite(DISTANCE_VIRTUAL_CHANNEL, distance, "prox", "m");
 }
